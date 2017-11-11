2ac993c8382cf3b01d429429038d5c39375bc80f
alexandrtovmach <alexandrtovmach@gmail.com>
2017-11-11 20:10:24 +0200

    init commit

---
+++ frontend/src/app/components/index-page/index-page.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { MatDialog, MatDialogRef } from '@angular/material';
+import { Router } from '@angular/router';
+
+@Component({
+    selector: 'app-index-page',
+    templateUrl: './index-page.component.html',
+    styleUrls: ['./index-page.component.scss']
+})
+export class IndexPageComponent implements OnInit {
+    ngOnInit() {}
+}
